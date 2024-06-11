84f9a532d39843a007707871eb8cb91eb29c3a52
burak.yilmaz <burakyilmaz01@gmail.com>
2024-06-11 10:35:53 +0300

    jwtGenerator ready

--- EventPlanner.Application/Services/Authentication/AuthenticationService.cs
+++ EventPlanner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,14 +1,35 @@
+using EventPlanner.Application.Common.Interfaces.Authentication;
+
 namespace EventPlanner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
-    public AuthenticationResult Login(string email, string password)
-    {
-       return new AuthenticationResult(Guid.NewGuid(), "John", "Doe", email,"token");
-    }
+   private readonly IJwtTokenGenerator _jwtTokenGenerator;
 
-    public AuthenticationResult Register(string firstname, string lastname, string email, string password)
-    {
-       return new AuthenticationResult(Guid.NewGuid(), firstname, lastname, email,"token");
-    }
+   public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+   {
+      _jwtTokenGenerator = jwtTokenGenerator;
+   }
+
+   public AuthenticationResult Register(string firstname, string lastname, string email, string password)
+   {
+      //check if user already exists
+
+      //create user (generate unique id)
+      //generate token  
+      
+      Guid userId = Guid.NewGuid();
+      var token = _jwtTokenGenerator.GenerateToken(userId, firstname, lastname);
+      
+      return new AuthenticationResult(userId,
+          firstname,
+          lastname,
+          email,
+          token);
+   }
+   public AuthenticationResult Login(string email, string password)
+   {
+      return new AuthenticationResult(Guid.NewGuid(), "John", "Doe", email, "token");
+   }
+
 }
--- EventPlanner.Infrastructure/DependencyInjection.cs
+++ EventPlanner.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,19 @@
+using EventPlanner.Application.Common.Interfaces.Authentication;
+using EventPlanner.Application.Common.Interfaces.Services;
+using EventPlanner.Infrastructure.Authentication;
+using EventPlanner.Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.Extensions.Configuration;
 namespace EventPlanner.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+        ConfigurationManager configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         return services;
     }
 }
